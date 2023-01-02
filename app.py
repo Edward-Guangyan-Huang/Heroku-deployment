92f129af5798952440a68b8fef39eddce209305e
SammySuliman <96219622+SammySuliman@users.noreply.github.com>
2023-01-01 23:08:23 -0800

    changes

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, request,render_template
 import pickle
 from transitioner import Transitioner
 
-election_app = Flask(__name__)
+app = Flask(__name__)
 logmodel = pickle.load(open('logmodel.pkl', 'rb'))
 
-@election_app.route('/')
+@app.route('/')
 def home():
     return render_template('index2.html')
 
-@election_app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
@@ -24,4 +24,4 @@ def predict():
     return render_template('index2.html', prediction_text='Your choice for president should be {}'.format(output))
 
 if __name__ == "__main__":
-    election_app.run(debug=True)
+    app.run(debug=True)
